1f07d52e52f2bb8c23f266266f740ac0488da4ed
chou0728 <dean34520@gmail.com>
2019-11-18 00:46:29 +0800

    Updates

---
+++ precache-manifest.a2adae3c4e70aa95b2224005453cbdc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66628c13d8ed1b8b7520b67ef4904f06",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a16ea31536acb43d5a5",
+    "url": "/monsters-rolodex/static/css/main.1a30ebe0.chunk.css"
+  },
+  {
+    "revision": "fb4f97d19153daf30e92",
+    "url": "/monsters-rolodex/static/js/2.c7fc8d30.chunk.js"
+  },
+  {
+    "revision": "3a16ea31536acb43d5a5",
+    "url": "/monsters-rolodex/static/js/main.44f0c64e.chunk.js"
+  },
+  {
+    "revision": "0b89725c131e9628d0b6",
+    "url": "/monsters-rolodex/static/js/runtime-main.cc21738e.js"
+  }
+]);
